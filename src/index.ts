e5f73ebc640866a4b40640ffa02e621f4992bd3b
Denis Sakharov <denis.sakharov@icloud.com>
2024-11-23 18:05:27 +0400

    Add router

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { AutoRouter } from 'itty-router';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!!!');
-	},
-} satisfies ExportedHandler<Env>;
+const router = AutoRouter();
+
+router.get('/', () => ({ message: 'Hello, from itty-router v5.' }));
+
+router.get('/hi', () => ({ message: 'Hi, from itty-router v5.' }))
+
+export default router;
